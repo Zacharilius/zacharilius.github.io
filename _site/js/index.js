4fb0d3509c47007290a0415601b85c37d3826ac6
Zacharilius <zabensley@gmail.com>
2017-01-16 21:11:22 -0800

    Started adding star background to Seattle Skyline view (index.html)

--- _site/js/index.js
+++ _site/js/index.js
@@ -1,5 +1,6 @@
 $(function() {
 	setupHeight();
+    setupSky();
 })
 
 function setupHeight() {
@@ -12,3 +13,41 @@ function setupHeight() {
 function setPortfolioContainerHeight() {
 	$('.portfolio-container').height($(window).height() - 63) /* 64 is the height of the nav title */
 }
+
+/* Sky */
+
+function setupSky() {
+    var currentHoursInDay = getHoursOfDay();
+    var dayBreakHour = 6;  // TODO: Use real data
+    var nightTimeHour = 18;  // TODO: Use real data
+    if (currentHoursInDay >= dayBreakHour && currentHoursInDay <= nightTimeHour) {
+        setupDay();
+    } else {
+        setupNight();
+    }
+}
+
+function getHoursOfDay() {
+    var date = new Date();
+    return date.getHours();
+}
+
+function setupDay() {
+
+}
+
+function setupNight() {
+    var numberOfStars = 1000;
+    for (var i = 0; i < numberOfStars; i++) {
+        var star = $("<div class='star-five-container'><div class='star-five'></div></div>");
+        star.css('top', getRandomNumber($(window).height()));
+        star.css('left', getRandomNumber($(window).width() + 200) - 200); /* 200 is the star's width */
+        $('body').append(star);
+    }
+}
+
+function getRandomNumber(maxNumber) {
+    return Math.floor(Math.random() * maxNumber)
+}
+
+
