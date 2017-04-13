27360b1788c29331f88853050d985fea974dc5a0
Zacharilius <zabensley@gmail.com>
2017-04-13 04:01:36 -0700

    Restyled Park maps page. Added seattle park backgrounds to the park bench map.

--- static/js/map.js
+++ static/js/map.js
@@ -1,52 +1,55 @@
-// initialize the map
-var map = L.map('map').setView([47.6062, -122.3321], 11);
+/* ==== Park Bench map in Seattle ==== */
+
+var parkBenchesMap = L.map('parkBenchesMap').setView([47.6062, -122.3321], 11);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'zacharilius.033jgmma',
     accessToken: 'pk.eyJ1IjoiemFjaGFyaWxpdXMiLCJhIjoiY2luemR3cmlyMTh6M3U4a2pmb3ZtZnZyMSJ9.Q1CwCauCwWmQd7t-Z8S8qQ'
-}).addTo(map);
+}).addTo(parkBenchesMap);
 
-$.getJSON('/static/js/seattle_parks_rec.geojson', function (data) {
-    // Define the geojson layer and add it to the map
-	var geojsonMarkerOptions = {
-	    radius: 3,
-	    fillColor: "#ff7800",
-	    color: "#000",
-	    weight: 1,
-	    opacity: 1,
-	    fillOpacity: 0.8
-	};
+$.getJSON('/static/js/seattle_parks.geojson', function (data) {
+    var seattleParksStyle = {
+            stroke: false,
+            fillColor: '#31a354',
+            fillOpacity: 0.8
+        };
 
-	L.geoJson(data, {
-	    pointToLayer: function (feature, latlng) {
-	        return L.circleMarker(latlng, geojsonMarkerOptions);
-	    }
-	}).addTo(map);
+     var geojsonMarkerOptions = {
+         radius: 3,
+         fillColor: "#ff7800",
+         color: "#000",
+         weight: 1,
+         opacity: 1,
+         fillOpacity: 0.8
+     };
+
+     L.geoJson(data, {
+        onEachFeature: function(feature, layer) {
+            if (feature.geometry.type == 'Polygon') {
+                layer.setStyle(seattleParksStyle);
+            }
+        },
+         pointToLayer: function(feature, latlng) {
+             return L.circleMarker(latlng, geojsonMarkerOptions);
+         }
+     }).addTo(parkBenchesMap);
 });
+
+
 
 var legend = L.control({position: 'topright'});
 
 function getColor(category) {
-	return "#ff7800";
+	return "";
 }
 
 legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend');
-    div.innerHTML += "<p>Legend</p>";
-    var categories = ['Park Benches'];
-    var labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < categories.length; i++) {
-		div.innerHTML +=
-	        '<i class="circle" style="background:' + getColor(categories[i]) + '"></i> ' +
-	         (categories[i] ? categories[i] + '<br>' : '+');
-    }
-
-    return div;
+    var legendEl = $('<div class="info legend"><p>Legend</p></div>');
+    legendEl.append('<div><i class="circle"></i>Park Benches</div>');
+    legendEl.append('<div><i class="square"></i>City Park</div>')
+    return legendEl[0];
 };
 
-legend.addTo(map);
+legend.addTo(parkBenchesMap);
