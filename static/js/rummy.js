de93409f4c47ab79210f67cce1b197f67d6bd34c
Zacharilius <zabensley@gmail.com>
2017-08-07 19:07:19 -0700

    Basic statistics being displayed on who is winning the rummy game.

--- static/js/rummy.js
+++ static/js/rummy.js
@@ -3,7 +3,7 @@ $(function() {
 });
 
 function setupRummyPage() {
-
+    requestRummySheet()
 }
 
 function requestRummySheet() {
@@ -25,21 +25,59 @@ function summarizeData(data) {
     var deanaPoints = data[0].slice(1).map(strToNumb);
     var zachPoints = data[1].slice(1).map(strToNumb);
 
+    if (deanaPoints.length != zachPoints.length) {
+        throw Exception('Unequal number of points between Zach and Deana')
+    }
+
     // Total points
     var deanaTotalPoints = deanaPoints.reduce(getSum);
     var zachTotalPoints = zachPoints.reduce(getSum);
     var totalPoints = deanaTotalPoints + zachTotalPoints;
+    $('#z-total-points').text(zachTotalPoints);
+    $('#d-total-points').text(deanaTotalPoints);
+    $('#total-points').text(totalPoints);
+
+    // Current Winner
+    var currentWinner;
+    if (deanaTotalPoints > zachTotalPoints) {
+        currentWinner = 'Deana';
+    } else if (deanaTotalPoints < zachTotalPoints) {
+        currentWinner = 'Zach';
+    } else {
+        currentWinner = 'It\'s a tie!';
+    }
+    $('#current-winner').text(currentWinner);
 
     // Average points per trick
-    if (deanaPoints.length != zachPoints.length) {
-        throw Exception('Unequal number of points between Zach and Deana')
-    }
     var numberTricks = deanaPoints.length;  // Zach and Deana points are equal
     var deanaAveragePointsPerTrick = deanaTotalPoints / numberTricks;
     var zachAveragePointsPerTrick = zachTotalPoints / numberTricks;
-    var averagePointsPerTrick = (deanaTotalPoints + zachTotalPoints) / numberTricks;
+    var averagePointsPerTrick = totalPoints / numberTricks;
+    $('#z-avg-points-per-trick').text(zachAveragePointsPerTrick);
+    $('#d-avg-points-per-trick').text(deanaAveragePointsPerTrick);
+    $('#avg-points-per-trick').text(averagePointsPerTrick);
 
 
+    // Number tricks won
+    var numberOfTricksDeanaWon = 0;
+    var numberOfTricksZachWon = 0;
+    var numberOfTricksTied = 0;
+    for (var i = 0; i < deanaPoints.length; i++) {
+        if (deanaPoints[i] > zachPoints[i]) {
+            numberOfTricksDeanaWon += 1;
+        } else if (deanaPoints[i] < zachPoints[i]) {
+            numberOfTricksZachWon += 1;
+        } else {  // Equal
+            numberOfTricksTied += 1;
+        }
+    }
+    $('#z-num-tricks-won').text(numberOfTricksZachWon);
+    $('#d-num-tricks-won').text(numberOfTricksDeanaWon);
+    $('#num-tricks-won').text(numberOfTricksDeanaWon + numberOfTricksZachWon);
+
+    // Longest winning streak
+
+    // Line graph of current winner for each trick.
 }
 
 function getSum(total, num) {
