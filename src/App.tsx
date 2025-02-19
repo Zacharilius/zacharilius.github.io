454d30dc6343da6f578aa895a579ccf4d7aa1f19
Zachary Bensley <zabensley@gmail.com>
2025-02-18 19:49:23 -0500

    Add About, Nav, Projects, & Resume.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
+import React, { useState } from 'react'
 import './App.css';
+import About from './About';
+import Nav from './Nav';
+import Projects from './Projects';
+import Resume from './Resume';
+
+export interface NavigationItem {
+    name: string;
+    href: string;
+    // TODO: Find better type
+    component: React.ReactElement;
+}
+
+const navigationItems: NavigationItem[] = [
+  { name: 'About', href: '#about', component: <About/>},
+  { name: 'Projects', href: '#projects', component: <Projects/> },
+  { name: 'Resume', href: '#resume', component: <Resume/> },
+];
 
 const App = () => {
-  return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
-  );
+    const [navHref, setNavHref] = useState(navigationItems[0].href);
+
+    const getActiveComponent = (navHref: string): any => {
+        return navigationItems.find((navigationItem) => navigationItem.href === navHref)?.component
+    }
+
+    return (
+        <div className="bg-white">
+            <Nav
+                navigationItems={navigationItems}
+                setNavHref={setNavHref}
+            />
+            {/* <div>
+            {
+                navigationItems.map(navigationItem => {
+                    return navigationItem.component
+                })
+            }
+            </div> */}
+            { getActiveComponent(navHref) }
+        </div>
+    );
 };
 
 export default App;
---
+++ src/Resume/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Resume() {
+  return (
+    <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="text-center">
+            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+              Resume
+            </h1>
+        </div>
+      </div>
+    </div>
+  )
+}
