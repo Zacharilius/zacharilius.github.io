27776424e4018cb6c4cd73446391ba86c299d2e3
Zachary Bensley <zabensley@gmail.com>
2025-02-19 17:21:39 -0500

    Try /dist/

--- src/Projects/index.tsx
+++ src/Projects/index.tsx
@@ -6,7 +6,7 @@ export default function Projects() {
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div className="text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Projects
+              TODO: Projects Page
             </h1>
         </div>
       </div>
