0cf1e935a6d088c8a8e97672520225acfb2e5615
Zachary Bensley <zabensley@gmail.com>
2025-02-20 07:06:27 -0500

    Show resume.

--- src/Resume/index.tsx
+++ src/Resume/index.tsx
@@ -3,12 +3,12 @@ import React from "react"
 export default function Resume() {
   return (
     <div className="relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="text-center">
-            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              TODO: Resume Page
-            </h1>
-        </div>
+      <div className="mx-auto max-w-2xl">
+        <iframe
+          title="resume"
+          className="h-screen max-w-820px w-full"
+          src="https://docs.google.com/document/d/18fosfLiDGNl5dwqsXBVCPOclMdjrkeqjUDaXZWtLOW8/pub?embedded=true"
+        />
       </div>
     </div>
   )
